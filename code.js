af294ed1f796218bbc50ba6ed69313de57a043dc
lucasjanta <lucastyle2014@gmail.com>
2023-02-08 16:26:00 -0300

    Setando destaques

--- code.js
+++ code.js
@@ -1,20 +1,20 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://cors-anywhere.herokuapp.com/api.hgbrasil.com/weather?woeid=455881', true);
+// var request = new XMLHttpRequest();
+// request.open('GET', 'https://cors-anywhere.herokuapp.com/api.hgbrasil.com/weather?woeid=455881', true);
 
-request.onload = function (){
-    if (this.status >= 200 && this.status < 400) {
-        var data = JSON.parse(this.response);
-        console.log(data);
-    }else{
+// request.onload = function (){
+//     if (this.status >= 200 && this.status < 400) {
+//         var data = JSON.parse(this.response);
+//         console.log(data);
+//     }else{
         
-    }
+//     }
 
 
-}
+// }
 
-request.onerror = function(){
+// request.onerror = function(){
 
-}
+// }
 
-request.send();
+// request.send();
 
